e255875741e8a94b42fee71fb2a69d73dee689b0
Alden Reed <alden@catalysis.com>
2022-07-11 09:44:23 -0700

    added another sail

--- GodotPrototype/Node2D.gd
+++ GodotPrototype/Node2D.gd
@@ -9,17 +9,30 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _on_CheckBox2_toggled(button_pressed):
 	if (button_pressed):
-		self.find_node("Sail").get_child(0).show()
+		self.find_node("Sail").find_node("Firstsail").get_child(0).show()
 		self.find_node("Base").get_child(0).move_local_y(40)
 	else:
-		self.find_node("Sail").get_child(0).hide()
+		self.find_node("Sail").find_node("Firstsail").get_child(0).hide()
 		self.find_node("Base").get_child(0).move_local_y(-40)
 
 
 func _on_CheckBox_toggled(button_pressed):
 	if (button_pressed):
 		self.find_node("Base").get_child(0).show()
-		self.find_node("Sail").get_child(0).move_local_y(-40)
+		for sails in self.find_node("Sail").get_children():
+			sails.get_child(0).move_local_y(-40)
+			#self.find_node("Sail").get_child(0).move_local_y(-40)
 	else:
 		self.find_node("Base").get_child(0).hide()
-		self.find_node("Sail").get_child(0).move_local_y(40)
+		for sails in self.find_node("Sail").get_children():
+			sails.get_child(0).move_local_y(40)
+			#self.find_node("Sail").get_child(0).move_local_y(40)
+
+
+func _on_CheckBox3_toggled(button_pressed):
+	if (button_pressed):
+		self.find_node("Sail").find_node("Othersail").get_child(0).show()
+		self.find_node("Base").get_child(0).move_local_y(40)
+	else:
+		self.find_node("Sail").find_node("Othersail").get_child(0).hide()
+		self.find_node("Base").get_child(0).move_local_y(-40)
