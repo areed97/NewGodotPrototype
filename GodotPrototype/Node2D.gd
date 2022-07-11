30daba0081b07d2c7a916c8ffb55fc679633b0aa
Alden Reed <alden@catalysis.com>
2022-07-11 10:01:12 -0700

    fixing bug

--- GodotPrototype/Node2D.gd
+++ GodotPrototype/Node2D.gd
@@ -8,12 +8,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _on_CheckBox2_toggled(button_pressed):
-	if (button_pressed):
-		self.find_node("Sail").find_node("Firstsail").get_child(0).show()
-		self.find_node("Base").get_child(0).move_local_y(40)
-	else:
-		self.find_node("Sail").find_node("Firstsail").get_child(0).hide()
-		self.find_node("Base").get_child(0).move_local_y(-40)
+	if(!self.get_node("CheckBox3").pressed):
+		if (button_pressed):
+			self.find_node("Sail").find_node("Firstsail").get_child(0).show()
+			self.find_node("Base").get_child(0).move_local_y(40)
+		else:
+			self.find_node("Sail").find_node("Firstsail").get_child(0).hide()
+			self.find_node("Base").get_child(0).move_local_y(-40)
 
 
 func _on_CheckBox_toggled(button_pressed):
@@ -30,9 +31,10 @@ func _on_CheckBox_toggled(button_pressed):
 
 
 func _on_CheckBox3_toggled(button_pressed):
-	if (button_pressed):
-		self.find_node("Sail").find_node("Othersail").get_child(0).show()
-		self.find_node("Base").get_child(0).move_local_y(40)
-	else:
-		self.find_node("Sail").find_node("Othersail").get_child(0).hide()
-		self.find_node("Base").get_child(0).move_local_y(-40)
+	if(!self.get_node("CheckBox2").pressed):
+		if (button_pressed):
+			self.find_node("Sail").find_node("Othersail").get_child(0).show()
+			self.find_node("Base").get_child(0).move_local_y(40)
+		else:
+			self.find_node("Sail").find_node("Othersail").get_child(0).hide()
+			self.find_node("Base").get_child(0).move_local_y(-40)
